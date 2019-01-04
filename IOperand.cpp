fdee5a872ffa86928f0e5bd1ea678e2cd444638b
bsemchuk <kubacortin@gmail.com>
2019-01-04 18:35:11 +0200

    initial commit

---
+++ IOperand.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Bogdan Semchuk on 1/4/19.
+//
+
+#include "IOperand.hpp"
+
---
+++ IOperand.hpp
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  IOperand.cpp
+ *
+ *    Description: 	Here are code copypasted from the subject, these pieces must be in
+ *    				my own code
+ *
+ *        Version:  1.0
+ *        Created:  01/04/19 16:31:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+class IOperand {
+
+public:
+	virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
+	virtual eOperandType getType( void ) const = 0; // Type of the instance
+	virtual IOperand const * operator+( IOperand const & rhs ) const = 0; // Sum
+	virtual IOperand const * operator-( IOperand const & rhs ) const = 0; // Difference
+	virtual IOperand const * operator*( IOperand const & rhs ) const = 0; // Product
+	virtual IOperand const * operator/( IOperand const & rhs ) const = 0; // Quotient
+	virtual IOperand const * operator%( IOperand const & rhs ) const = 0; // Modulo
+	virtual std::string const & toString( void ) const = 0; // String representation of the instance
+	virtual ~IOperand( void ) {}
+
+private:
+    IOperand const * createInt8( std::string const & value ) const;
+    IOperand const * createInt16( std::string const & value ) const;
+    IOperand const * createInt32( std::string const & value ) const;
+    IOperand const * createFloat( std::string const & value ) const;
+    IOperand const * createDouble( std::string const & value ) const;
+};
